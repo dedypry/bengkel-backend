dd0e9d5818e8e7c76a69f7589025dbe1032bef42
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2026-04-21 06:02:07 +0700

    update

--- db/seeds/08_reset_password.ts
+++ db/seeds/08_reset_password.ts
@@ -5,7 +5,7 @@ export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   const hashedPassword = await bcrypt.hash('hcp@2026', 10);
 
-  await knex('users').whereNot('email', 'admin@bengkel.com').update({
+  await knex('users').where('email', 'tri@gmail.com').update({
     password: hashedPassword,
   });
 }
