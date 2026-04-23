c70e9ea1d1ca3fbbea87ba267da20f21b83ea6c4
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2026-04-23 14:44:23 +0700

    update

--- apps/whatsapp/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { WhatsappModule } from './whatsapp.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(WhatsappModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
